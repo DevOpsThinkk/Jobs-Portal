31f5dd0f5264479a7ff24f533a77528c1a620d6c
DevOpsGH <devopsthink@gmail.com>
2020-06-10 13:09:38 +0530

    updated readme.md file

--- Steps_after_prerequisites.sh
+++
@@ -1,8 +0,0 @@
-
-cd /home/django
-git clone https://github.com/DevOpsGH/Jobs-Portal.git
-mv Jobs-Portal myproject
-source /home/django/myprojectenv/bin/activate
-cd myproject
-pip3 install -r requirements.txt
-
